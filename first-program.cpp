33e2ea76a0359da335c47dcd4da181f5a489fc06
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-08-03 12:48:48 -0400

    First commit

---
+++ first-program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int base, altura,eduardo=0;
+	cout<<"Ingrese la base: "; cin>>base;
+	cout<<"Ingrese la altura "; cin>>altura;
+	eduardo = (base*altura)/2;
+	cout<<"El resultado es "<<eduardo;
+	getch();
+	return 0;
+}
